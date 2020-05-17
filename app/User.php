e65c61acf7565dcb785e3254163bb0330b76bb6e
Ayush Mukherjee <ayushm123460@gmail.com>
2020-05-17 23:55:00 +0530

    fix usage of trait, add default admin user seeder

--- app/User.php
+++ app/User.php
@@ -9,7 +9,7 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     use Notifiable;
-    use App\Traits\UsesUuid;
+    use \App\Traits\UsesUuid;
 
     /**
      * The attributes that are mass assignable.
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $uuid = Str::uuid();
+
+        DB::table('users')->insert([
+            'uuid' => $uuid,
+            'fname' => 'Admin',
+            'lname' => 'Admin',
+            'email' => 'admin@domain.com',
+            'password' => bcrypt('passw0rd'),
+            'role' => 'admin',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('admins')->insert([
+            'uuid' => $uuid,
+            'empno' => '00001',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+    }
+}
